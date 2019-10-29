d0cc4741b5a960590f5d3424fc4a13f9988057c8
dzendev <dzendev@gmail.com>
2019-10-29 13:09:51 +0400

    init and array/first.js

---
+++ array/first.js
@@ -0,0 +1,16 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+const arr = [5, 4, 3, 2, 1];
+
+// underscore
+console.log("und.first(arr)", und.first(arr));
+console.log("und.head(arr)", und.head(arr));
+console.log("und.take(arr)", und.take(arr));
+
+// lodash
+console.log("lod.first(arr)", lod.first(arr));
+console.log("lod.head(arr)", lod.head(arr));
+
+// es6
+console.log("arr[0]", arr[0]);
