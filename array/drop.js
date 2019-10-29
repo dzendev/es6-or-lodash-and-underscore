f5f046acc64cfbf2c16202c7e73019a3367f4ace
dzendev <dzendev@gmail.com>
2019-10-29 15:06:21 +0400

    add array/drop-rest-tail

---
+++ array/drop.js
@@ -0,0 +1,26 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * drop - возвращает часть массива
+ * _.drop([5, 4, 3, 2, 1], 2) -> [ 3, 2, 1 ]
+ */
+
+const arr = [5, 4, 3, 2, 1];
+
+// underscore
+console.log("und.drop(arr)", und.drop(arr));
+console.log("und.drop(arr, 2)", und.drop(arr, 2));
+console.log("und.drop(arr, 0)", und.drop(arr, 0));
+console.log("und.rest(arr)", und.rest(arr)); // alias
+console.log("und.tail(arr)", und.tail(arr)); // alias
+
+// lodash
+console.log("lod.drop(arr)", lod.drop(arr));
+console.log("lod.drop(arr, 2)", lod.drop(arr, 2));
+console.log("lod.drop(arr, 0)", lod.drop(arr, 0));
+
+// es6
+console.log("arr.slice(1)", arr.slice(1));
+console.log("arr.slice(2)", arr.slice(2));
+console.log("arr.slice()", arr.slice());
