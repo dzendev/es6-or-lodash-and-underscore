b9d3c4e9b03b04cab3300641570d188628d2257e
dzendev <dzendev@gmail.com>
2019-10-30 12:54:45 +0400

    add array/reverse

---
+++ array/reverse.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * reverse - изменяет порядок следования элементов на обратный
+ * _.reverse([1, 2, 3, 4]) -> [ 4, 3, 2, 1 ]
+ */
+
+// lodash
+console.log("lod.reverse([1, 2, 3, 4])", lod.reverse([1, 2, 3, 4]));
+
+// es6
+console.log("[1, 2, 3, 4].reverse()", [1, 2, 3, 4].reverse());
