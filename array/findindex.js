ff52f76a299b2f764136d00719eb234aeb01afb3
dzendev <dzendev@gmail.com>
2019-10-30 18:31:30 +0400

    change array/findindex

--- array/findindex.js
+++ array/findindex.js
@@ -3,7 +3,7 @@ const lod = require('lodash');
 
 /**
  * findIndex - ищет ндекс совпадения
- * _.findIndex([['x', 1], ['y', 2]]) -> { x: 1, y: 2 }
+ * _.findIndex([{'x':1}, {'x':2}, {'x':3}], {'x': 2}) -> 1
  */
 
 // underscore
