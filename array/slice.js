ac2aa23a5a85cb7cb1d5fce13cd8879f36e786b8
dzendev <dzendev@gmail.com>
2019-10-30 12:58:28 +0400

    add array/slice

---
+++ array/slice.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * slice - возвращает часть массива
+ * _.slice([1, 2, 3, 4]) -> [ 2 ]
+ */
+
+// lodash
+console.log("lod.slice([1, 2, 3, 4], 1, 2)", lod.slice([1, 2, 3, 4], 1, 2));
+
+// es6
+console.log("[1, 2, 3, 4].slice(1, 2)", [1, 2, 3, 4].slice(1, 2));
