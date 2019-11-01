5a78a208792add28572260bf0ed300fc73a74bbf
dzendev <dzendev@gmail.com>
2019-11-01 16:43:05 +0400

    add array/take

---
+++ array/take.js
@@ -0,0 +1,17 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * take - часть массива
+ * _.take([1, 2, 3]) -> [ 1, 2 ]
+ */
+
+// lodash
+console.log("lod.take([1, 2, 3])", lod.take([1, 2, 3]));
+console.log("lod.take([1, 2, 3], 2)", lod.take([1, 2, 3], 2));
+console.log("lod.take([1, 2, 3], 5)", lod.take([1, 2, 3], 5));
+
+// es6
+console.log("[1, 2, 3].slice(0, 1)", [1, 2, 3].slice(0, 1));
+console.log("[1, 2, 3].slice(0, 2)", [1, 2, 3].slice(0, 2));
+console.log("[1, 2, 3].slice(0, 5)", [1, 2, 3].slice(0, 5));
