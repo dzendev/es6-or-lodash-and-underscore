290ae14188773bd7fe3f1258946cd3dcebe9d388
dzendev <dzendev@gmail.com>
2019-11-05 11:15:59 +0400

    change collection/size

---
+++ collection/size.js
@@ -0,0 +1,18 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * size - длина коллекции
+ * lod.size({ 'a': 1, 'b': 2 }) -> 2
+ */
+
+
+// lodash
+console.log("lod.size([1, 2, 3])", lod.size([1, 2, 3]));
+console.log("lod.size({ 'a': 1, 'b': 2 })", lod.size({ 'a': 1, 'b': 2 }));
+console.log("lod.size('pebbles')", lod.size('pebbles'));
+
+// es6
+console.log("[1, 2, 3].length", [1, 2, 3].length);
+console.log("Object.keys({'a':1, 'b':2}).length", Object.keys({'a':1, 'b':2}).length);
+console.log("'pabbles'.length", 'pabbles'.length);
