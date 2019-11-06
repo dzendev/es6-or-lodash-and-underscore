239d4fd4a551b5b01824e7bb3945603fff812b78
dzendev <dzendev@gmail.com>
2019-11-06 20:13:20 +0400

    add math/subtract

---
+++ math/subtract.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * subtract - вычитание
+ * _.subtract(6, 4) -> 2
+ */
+
+// lodash
+console.log("lod.subtract(6, 4)", lod.subtract(6, 4));
+
+// es6
+console.log("6 - 4", 6 - 4);
