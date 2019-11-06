e057037eb1abc407b25aa7a56b2d153fe5ac168f
dzendev <dzendev@gmail.com>
2019-11-06 18:35:24 +0400

    add math/add

---
+++ math/add.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * add - складывание 2-х чисел
+ * _.add(4, 6) -> 10
+ */
+
+// lodash
+console.log("lod.add(4, 6)", lod.add(4, 6));
+
+// es6
+console.log("4 + 6", 4 + 6);
