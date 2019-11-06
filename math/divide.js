0139497517c06e1734292f89390a86577ec09c92
dzendev <dzendev@gmail.com>
2019-11-06 18:39:32 +0400

    add math/divide

---
+++ math/divide.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * divide - разделить два числа
+ * _.divide(6, 4) -> 1.5
+ */
+
+// lodash
+console.log("lod.divide(6, 4)", lod.divide(6, 4));
+
+// es6
+console.log("6 / 4", 6 / 4);
