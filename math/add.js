8c473be29f43a55bfd89db94467c39041f873b7a
dzendev <dzendev@gmail.com>
2019-11-07 10:06:56 +0400

    change math/add

--- math/add.js
+++ math/add.js
@@ -8,6 +8,8 @@ const lod = require('lodash');
 
 // lodash
 console.log("lod.add(4, 6)", lod.add(4, 6));
+console.log("lod.add('4', '6')", lod.add('4', '6'));
 
 // es6
 console.log("4 + 6", 4 + 6);
+console.log("'4' + '6'", '4' + '6');
