8e4f77d266552a6ab3bf9987465a9b897c4909ac
dzendev <dzendev@gmail.com>
2019-11-07 10:01:37 +0400

    add math/multiply

---
+++ math/multiply.js
@@ -0,0 +1,13 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * multiply - умножение
+ * _.multiply(6, 4) -> 24
+ */
+
+// lodash
+console.log("lod.multiply(6, 4)", lod.multiply(6, 4));
+
+// es6
+console.log("6 * 4", 6 * 4);
