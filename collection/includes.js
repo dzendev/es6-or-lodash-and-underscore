2f376f078adbe1bcb75d46d6af9d169b4031f136
dzendev <dzendev@gmail.com>
2019-11-08 12:24:00 +0400

    add collection/includes

--- collection/includes.js
+++ collection/includes.js
@@ -2,24 +2,18 @@ const und = require('underscore');
 const lod = require('lodash');
 
 /**
- * forEach - перебор коллекции
- * _.forEach([1, 2], value => value * 2) -> 2 4
+ * includes - возвращает отсортированный массив
+ * _.includes([1, 2, 3], 1) -> true
  */
 
 // lodash
-lod.forEach([1, 2], function(value) {
-  console.log(value * 2);
-});
-lod.forEach({ 'a': 1, 'b': 2 }, function(value, key) {
-  console.log(key);
-});
+console.log("lod.includes([1, 2, 3], 1)", lod.includes([1, 2, 3], 1));
+console.log("lod.includes([1, 2, 3], 1, 2)", lod.includes([1, 2, 3], 1, 2));
+console.log("lod.includes({ 'a': 1, 'b': 2 }, 1)", lod.includes({ 'a': 1, 'b': 2 }, 1));
+console.log("lod.includes('abcd', 'bc')", lod.includes('abcd', 'bc'));
 
 // es6
-[1, 2].forEach(value => console.log(value * 2))
-Object.keys({ 'a': 1, 'b': 2 }).forEach(value => console.log(value));
-let obj = { 'a': 1, 'b': 2 };
-for (let prop in obj) {
-	if (obj.hasOwnProperty(prop)) {
-		console.log(prop)
-	}
-}
+console.log("[1, 2, 3].includes(1)", [1, 2, 3].includes(1));
+console.log("[1, 2, 3].includes(1, 2)", [1, 2, 3].includes(1, 2));
+console.log("Object.values({'a':1, 'b':2}).includes(1)", Object.values({'a':1, 'b':2}).includes(1));
+console.log("'abcd'.includes('bc')", 'abcd'.includes('bc'));
