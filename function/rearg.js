44c9c0185353501733cb34dcce3b0d792be3736f
dzendev <dzendev@gmail.com>
2019-11-12 18:10:57 +0400

    add function/rearg

---
+++ function/rearg.js
@@ -0,0 +1,30 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * rearg - создаёт функцию с изменённым порядком аргументов
+ */
+
+// lodash
+var rearged = lod.rearg(function(a, b, c) {
+	return [a, b, c];
+}, [2, 0, 1]);
+
+console.log("rearged('b', 'c', 'a')", rearged('b', 'c', 'a'));
+
+// es6
+function rearg(callback, sequence) {
+	return function(...arg) {
+		let reargArg = [];
+		sequence.forEach((i) => {
+			reargArg.push(arg[i]);
+		});
+		return callback(...reargArg);
+	};
+}
+
+var rearged1 = lod.rearg(function(a, b, c) {
+	return [a, b, c];
+}, [2, 0, 1]);
+
+console.log("rearged1('b', 'c', 'a')", rearged1('b', 'c', 'a'));
