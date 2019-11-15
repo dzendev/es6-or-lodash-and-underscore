b9a82c947ae2db4c6c2f29b9251a622a96046db9
dzendev <dzendev@gmail.com>
2019-11-15 14:21:36 +0400

    add math/floor

---
+++ math/floor.js
@@ -0,0 +1,77 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * floor - округляет число в меньшую стороун
+ */
+
+// lodash
+console.log("lod.floor(4.006)", lod.floor(4.006));
+console.log("lod.floor(0.046, 2)", lod.floor(0.046, 2));
+console.log("lod.floor(4060, -2)", lod.floor(4060, -2));
+
+// es6
+function floor(value, decimals) {
+	if(decimals === undefined)
+		return Math.floor(value);
+	else
+		return Number(Math.floor(value+'e'+decimals)+'e-'+decimals);
+}
+
+console.log("floor(4.006)", floor(4.006));
+console.log("floor(0.046, 2)", floor(0.046, 2));
+console.log("floor(4060, -2)", floor(4060, -2)); // NaN
+
+// Решение с MDN
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+(function() {
+	/**
+	 * Корректировка округления десятичных дробей.
+	 *
+	 * @param {String}  type  Тип корректировки.
+	 * @param {Number}  value Число.
+	 * @param {Integer} exp   Показатель степени (десятичный логарифм основания корректировки).
+	 * @returns {Number} Скорректированное значение.
+	 */
+	function decimalAdjust(type, value, exp) {
+		// Если степень не определена, либо равна нулю...
+		if (typeof exp === 'undefined' || +exp === 0) {
+			return Math[type](value);
+		}
+		value = +value;
+		exp = +exp;
+		// Если значение не является числом, либо степень не является целым числом...
+		if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+			return NaN;
+		}
+		// Сдвиг разрядов
+		value = value.toString().split('e');
+		value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+		// Обратный сдвиг
+		value = value.toString().split('e');
+		return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+	}
+
+	// Десятичное округление к ближайшему
+	if (!Math.round10) {
+		Math.round10 = function(value, exp) {
+			return decimalAdjust('round', value, exp);
+		};
+	}
+	// Десятичное округление вниз
+	if (!Math.floor10) {
+		Math.floor10 = function(value, exp) {
+			return decimalAdjust('floor', value, exp);
+		};
+	}
+	// Десятичное округление вверх
+	if (!Math.ceil10) {
+		Math.ceil10 = function(value, exp) {
+			return decimalAdjust('ceil', value, exp);
+		};
+	}
+})();
+
+console.log("Math.floor10(4.006)", Math.floor10(4.006));
+console.log("Math.floor10(0.046, -2)", Math.floor10(0.046, -2));
+console.log("Math.floor10(4060, 2)", Math.floor10(4060, 2));
