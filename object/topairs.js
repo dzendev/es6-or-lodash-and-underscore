dc5d4f877d4748b34c748bd95b950cfb7b519d98
dzendev <dzendev@gmail.com>
2019-11-17 15:06:11 +0400

    change object/topairs

--- object/topairs.js
+++ object/topairs.js
@@ -6,21 +6,16 @@ const lod = require('lodash');
  * _.toPairs({ x: 1, y: 2 }) -> [['x', 1], ['y', 2]]
  */
 
-// lodash
-console.log("lod.toPairs({x:1, y:2})", lod.toPairs({x:1, y:2}));
-console.log("lod.entries({x:1, y:2})", lod.toPairs({x:1, y:2})); // alias
-
-// es6
-console.log("Object.entries({x:1, y:2})", Object.entries({x:1, y:2}));
-
-function toPairs(obj) {
-	let arr = [];
-	for(let prop in obj) {
-		if(obj.hasOwnProperty(prop)) {
-			arr.push([prop, obj[prop]]);
-		}
-	}
-	return arr;
+function Foo() {
+  this.a = 1;
+  this.b = 2;
 }
 
-console.log("toPairs({x:1, y:2})", toPairs({x:1, y:2}));
+Foo.prototype.c = 3;
+
+// lodash
+console.log("lod.toPairs(new Foo)", lod.toPairs(new Foo));
+console.log("lod.entries(new Foo)", lod.entries(new Foo)); // alias
+
+// es6
+console.log("Object.entries(new Foo)", Object.entries(new Foo));
