fa5a82dd993ae50e2698656e58aaa14d36047d50
dzendev <dzendev@gmail.com>
2019-11-27 13:41:25 +0400

    add string/words

---
+++ string/words.js
@@ -0,0 +1,18 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * words - разбивает строку на слова и возвращает массив
+ */
+
+// lodash
+console.log("lod.words('fred, barney, & pebbles')", lod.words('fred, barney, & pebbles'));
+console.log("lod.words('fred, barney, & pebbles', /[^, ]+/g)", lod.words('fred, barney, & pebbles', /[^, ]+/g));
+console.log("lod.words('яблоки, груши, сливы')", lod.words('яблоки, груши, сливы'));
+
+// es6
+console.log("'fred, barney, & pebbles'.match(/[^, &]+/g)", 'fred, barney, & pebbles'.match(/[^, &]+/g));
+console.log("'fred, barney, & pebbles'.split(' ')", 'fred, barney, & pebbles'.split(' '));
+console.log("'fred, barney, & pebbles'.match(/[^, ]+/g)", 'fred, barney, & pebbles'.match(/[^, ]+/g));
+'яблоки, груши, сливы'.split(' ')
+console.log("'яблоки, груши, сливы'.split(' ')", 'яблоки, груши, сливы'.split(' '));
