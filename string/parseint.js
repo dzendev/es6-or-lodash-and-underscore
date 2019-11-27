d9a3761cca24e85a8ac7c12f6c33f0a4c0a23f49
dzendev <dzendev@gmail.com>
2019-11-27 13:30:42 +0400

    add string/parseint

---
+++ string/parseint.js
@@ -0,0 +1,12 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * parseInt - преобразует строку к типу integer
+ */
+
+// lodash
+console.log("lod.parseInt('08')", lod.parseInt('08'));
+
+// es6
+console.log("parseInt('08')", parseInt('08'));
