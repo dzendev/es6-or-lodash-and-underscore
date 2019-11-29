7dfab7182a0f0ca68f5587eabf2597c866d3aa1c
dzendev <dzendev@gmail.com>
2019-11-29 10:32:19 +0400

    add string/deburr

---
+++ string/deburr.js
@@ -0,0 +1,19 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * deburr - нормализации юникод строки
+ */
+
+// lodash
+console.log("lod.deburr('déjà vu')", lod.deburr('déjà vu'));
+console.log("lod.deburr('ąśćńżółźćę')", lod.deburr('ąśćńżółźćę'));
+
+// es6
+function deburr(str) {
+	str = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+	return str.replace(/\u0142/g, "l");
+}
+
+console.log("deburr('déjà vu')", deburr('déjà vu'));
+console.log("deburr('ąśćńżółźćę')", deburr('ąśćńżółźćę'));
