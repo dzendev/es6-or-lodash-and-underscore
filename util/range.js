00eb2b44d1525a845c70633a5a276fa5c4a80724
dzendev <dzendev@gmail.com>
2019-12-02 12:57:56 +0400

    change util/range

--- util/range.js
+++ util/range.js
@@ -71,6 +71,8 @@ console.log("range(1, 5)", range(1, 5));
 console.log("range(1, -5)", range(1, -5));
 console.log("range(0, 20, 5)", range(0, 20, 5));
 console.log("range(0, -4, -1)", range(0, -4, -1));
+console.log("range(0, 4, 1)", range(0, 4, 1));
+console.log("range(1, 5, 1)", range(1, 5, 1));
 console.log("range(1, 4, 0)", range(1, 4, 0));
 console.log("range(0, 4, 0)", range(0, 4, 0));
 console.log("range(0)", range(0));
