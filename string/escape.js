447a03c887da9a946a421c8edb8b0e994c97faf2
dzendev <dzendev@gmail.com>
2020-02-12 18:43:18 +0400

    add string/escape

---
+++ string/escape.js
@@ -0,0 +1,33 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * escape - преобразует строку в нижний регистр, первый символ в верхний
+ */
+
+// lodash
+console.log("lod.escape('fred, barney, & pebbles')", lod.escape('fred, barney, & pebbles'));
+console.log("lod.escape('fred, barney, < pebbles')", lod.escape('fred, barney, < pebbles'));
+console.log("lod.escape('fred, barney, > pebbles')", lod.escape('fred, barney, > pebbles'));
+console.log("lod.escape('fred, barney, \" pebbles')", lod.escape('fred, barney, " pebbles'));
+console.log("lod.escape(\"fred, barney, ' pebbles\")", lod.escape("fred, barney, ' pebbles"));
+
+// es6
+function escape(str) {
+	let arr = [
+		["&", "&amp;"],
+		["<", "&lt;"],
+		[">", "&gt;"],
+		['"', "&quot;"],
+		["'", "&#39;"]
+	];
+	arr.forEach((el, index) => {
+		str = str.replace(el[0], el[1]);
+	});
+	return str;
+}
+console.log("escape('fred, barney, & pebbles')", escape('fred, barney, & pebbles'));
+console.log("escape('fred, barney, < pebbles')", escape('fred, barney, < pebbles'));
+console.log("escape('fred, barney, > pebbles')", escape('fred, barney, > pebbles'));
+console.log("escape('fred, barney, \" pebbles')", escape('fred, barney, " pebbles'));
+console.log("escape(\"fred, barney, ' pebbles\")", escape("fred, barney, ' pebbles"));
