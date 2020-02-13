d8bfad2c0a58acdc745a0b85b09987a2f11c17e6
dzendev <dzendev@gmail.com>
2020-02-13 21:04:05 +0400

    change string/escape.js

--- string/escape.js
+++ string/escape.js
@@ -6,13 +6,11 @@ const lod = require('lodash');
  */
 
 // lodash
-console.log("lod.escape('fred, barney, & pebbles')", lod.escape('fred, barney, & pebbles'));
-console.log("lod.escape('fred, barney, < pebbles')", lod.escape('fred, barney, < pebbles'));
-console.log("lod.escape('fred, barney, > pebbles')", lod.escape('fred, barney, > pebbles'));
-console.log("lod.escape('fred, barney, \" pebbles')", lod.escape('fred, barney, " pebbles'));
-console.log("lod.escape(\"fred, barney, ' pebbles\")", lod.escape("fred, barney, ' pebbles"));
+console.log(`lod.escape(\" & < > \\ ' текст text\")`, lod.escape(" & < > \\ ' текст text"));
 
 // es6
+
+// Вариант 1
 function escape(str) {
 	let arr = [
 		["&", "&amp;"],
@@ -26,8 +24,25 @@ function escape(str) {
 	});
 	return str;
 }
-console.log("escape('fred, barney, & pebbles')", escape('fred, barney, & pebbles'));
-console.log("escape('fred, barney, < pebbles')", escape('fred, barney, < pebbles'));
-console.log("escape('fred, barney, > pebbles')", escape('fred, barney, > pebbles'));
-console.log("escape('fred, barney, \" pebbles')", escape('fred, barney, " pebbles'));
-console.log("escape(\"fred, barney, ' pebbles\")", escape("fred, barney, ' pebbles"));
+console.log(`escape(\" & < > \\ ' текст text\")`, escape(" & < > \\ ' текст text"));
+
+// Вариант 2 - работает только в браузере
+function htmlentities(str) {
+	var textarea = document.createElement("textarea");
+	textarea.innerHTML = str;
+	return textarea.innerHTML;
+}
+// console.log(`htmlentities(" & < > \\ ' текст text")`, htmlentities('\" & < > \' текст text'));
+
+// Вариант 3
+function htmlspecialchars(str) {
+	var map = {
+		"&": "&amp;",
+		"<": "&lt;",
+		">": "&gt;",
+		"\"": "&quot;",
+		"'": "&#39;" // ' -> &apos; for XML only
+	};
+	return str.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+console.log(`htmlspecialchars(" & < > \\ ' текст text")`, htmlspecialchars('\" & < > \' текст text'));
