5fb2025ba8dd0e5bb5b0642db8f9bcd37ffdd415
dzendev <dzendev@gmail.com>
2020-02-15 10:37:05 +0400

    add collectio/countby.js

---
+++ collection/countby.js
@@ -0,0 +1,35 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * conutBy - создаёт объект. ключи получены через вызов функции, значения получены количеством раз вызова фукнции
+ * _.countBy([6.1, 4.2, 6.3], Math.floor) -> {'4': 1, '6': 2}
+ */
+
+// lodash
+console.log("lod.countBy([6.1, 4.2, 6.3], Math.floor)", lod.countBy([6.1, 4.2, 6.3], Math.floor));
+console.log("lod.countBy(['one', 'two', 'three'], 'length')", lod.countBy(['one', 'two', 'three'], 'length'));
+
+// es6
+function countBy(arr, fn) {
+	let obj = {};
+	arr.forEach(el => {
+		let key = undefined;
+		if(typeof fn === 'function') {
+			key = fn(el);
+		} else if(typeof fn === 'string'){
+			key = el[fn];
+		} else {
+			throw new Error('Неверный тип');
+		}
+		if(key in obj) {
+			obj[key]++;
+		} else {
+			obj[key] = 1;
+		}
+	});
+	return obj;
+}
+
+console.log("countBy([6.1, 4.2, 6.3], Math.floor)", countBy([6.1, 4.2, 6.3], Math.floor));
+console.log("countBy(['one', 'two', 'three'], length)", countBy(['one', 'two', 'three'], 'length'));
