98d877a0daedcc32af6949b3e7d60d8a219d4bee
dzendev <dzendev@gmail.com>
2020-02-25 10:32:01 +0400

    change object/has.js

--- object/has.js
+++ object/has.js
@@ -12,9 +12,8 @@ var other = lod.create({ 'a': lod.create({ 'b': 2 }) });
 console.log("lod.has(object, 'a')", lod.has(object, 'a'));
 console.log("lod.has(object, 'a.b')", lod.has(object, 'a.b'));
 console.log("lod.has(other, 'a')", lod.has(other, 'a'));
-console.log("lod.has(object, ['a', 'b'])", lod.has(object, ['a', 'b']));
 
 // es6
 console.log("object.hasOwnProperty('a')", object.hasOwnProperty('a'));
-console.log("object.a.hasOwnProperty('c')", object.a.hasOwnProperty('c'));
+console.log("object.a.hasOwnProperty('b')", object.a.hasOwnProperty('b'));
 console.log("other.hasOwnProperty('a')", other.hasOwnProperty('a'));
