d7ba0ca0e46c137fd80004d9b9913b5a1e9c86f9
dzendev <dzendev@gmail.com>
2020-02-28 16:15:51 +0400

    add lang/isweakmap.js

---
+++ lang/isweakmap.js
@@ -0,0 +1,19 @@
+const und = require('underscore');
+const lod = require('lodash');
+
+/**
+ * isWeakMap - проверяет является ли выражение WeakMap
+ */
+
+// lodash
+console.log("lod.isWeakMap(new WeakMap)", lod.isWeakMap(new WeakMap));
+console.log("lod.isWeakMap(new Map)", lod.isWeakMap(new Map));
+
+// es6
+function isWeakMap(value) {
+	if(Object.prototype.toString.call(value) === '[object WeakMap]') return true;
+	return false;
+}
+
+console.log("isWeakMap(new WeakMap)", isWeakMap(new WeakMap));
+console.log("isWeakMap(new Map)", isWeakMap(new Map));
