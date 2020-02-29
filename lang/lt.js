6c642cf084d3333471ac24c97fb9315f19909f34
dzendev <dzendev@gmail.com>
2020-02-29 10:42:08 +0400

    change lang/lt.js

--- lang/lt.js
+++ lang/lt.js
@@ -15,6 +15,6 @@ function lt(el1, el2) {
 	return el1 < el2;
 }
 
-console.log("lod.lt(1, 3)", lod.lt(1, 3));
-console.log("lod.lt(3, 3)", lod.lt(3, 3));
-console.log("lod.lt(3, 1)", lod.lt(3, 1));
+console.log("lt(1, 3)", lt(1, 3));
+console.log("lt(3, 3)", lt(3, 3));
+console.log("lt(3, 1)", lt(3, 1));
