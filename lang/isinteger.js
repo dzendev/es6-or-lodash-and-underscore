c01dd0453cb87cfa201abbfa6e6267192ac29723
dzendev <dzendev@gmail.com>
2020-03-05 21:01:00 +0400

    change lang/isinteger.js

--- lang/isinteger.js
+++ lang/isinteger.js
@@ -16,3 +16,8 @@ console.log("parseInt(3) === 3", parseInt(3) === 3);
 console.log("parseInt(Number.MIN_VALUE) === Number.MIN_VALUE", parseInt(Number.MIN_VALUE) === Number.MIN_VALUE);
 console.log("parseInt(Infinity) === Infinity", parseInt(Infinity) === Infinity);
 console.log("parseInt('3') === '3'", parseInt('3') === '3');
+
+console.log("Number.isInteger(3)", Number.isInteger(3));
+console.log("Number.isInteger(Number.MIN_VALUE)", Number.isInteger(Number.MIN_VALUE));
+console.log("Number.isInteger(Infinity)", Number.isInteger(Infinity));
+console.log("Number.isInteger('3')", Number.isInteger('3'));
