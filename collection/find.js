ca6f52e3c1d3c01cb01093792dfc6242bc9f5226
dzendev <dzendev@gmail.com>
2020-04-02 12:29:50 +0400

    change collection/find.js

--- collection/find.js
+++ collection/find.js
@@ -11,6 +11,12 @@ var users = [
   { 'user': 'pebbles', 'age': 1,  'active': true }
 ];
 
+// underscore
+console.log("und.find(users, function(o) { return o.age < 40; })", und.find(users, function(o) { return o.age < 40; }));
+console.log("und.find(users, { 'age': 1, 'active': true })", und.find(users, { 'age': 1, 'active': true }));
+console.log("und.find(users, ['active', false])", und.find(users, ['active', false])); // undefined
+console.log("und.find(users, 'active')", und.find(users, 'active'));
+
 // lodash
 console.log("lod.find(users, function(o) { return o.age < 40; })", lod.find(users, function(o) { return o.age < 40; }));
 console.log("lod.find(users, { 'age': 1, 'active': true })", lod.find(users, { 'age': 1, 'active': true }));
