3cc46c88700b7f061f832cb31c97815c30d3fac3
dzendev <dzendev@gmail.com>
2020-04-02 12:21:40 +0400

    change collection/map.js

--- collection/map.js
+++ collection/map.js
@@ -6,24 +6,26 @@ const lod = require('lodash');
  * _.map([4, 8], n => n * n) -> [ 16, 64 ]
  */
 
-
-// lodash
 function square(n) {
 	return n * n;
 }
-console.log("lod.map([4, 8], square)", lod.map([4, 8], square));
-
-console.log("lod.map({ 'a': 4, 'b': 8 }, square)", lod.map({ 'a': 4, 'b': 8 }, square));
 
 var users = [
 	{ 'user': 'barney' },
 	{ 'user': 'fred' }
 ];
+
+// underscore
+console.log("und.map([4, 8], square)", und.map([4, 8], square));
+console.log("und.map({ 'a': 4, 'b': 8 }, square)", und.map({ 'a': 4, 'b': 8 }, square));
+console.log("und.map(users, 'user')", und.map(users, 'user'));
+
+// lodash
+console.log("lod.map([4, 8], square)", lod.map([4, 8], square));
+console.log("lod.map({ 'a': 4, 'b': 8 }, square)", lod.map({ 'a': 4, 'b': 8 }, square));
 console.log("lod.map(users, 'user')", lod.map(users, 'user'));
 
 // es6
 console.log("[4, 8].map(n => n * n)", [4, 8].map(n => n * n));
-
 console.log("Object.values({ 'a': 4, 'b': 8 }).map(n => n * n)", Object.values({ 'a': 4, 'b': 8 }).map(n => n * n));
-
 console.log("users.map(el => el.user)", users.map(el => el.user));
