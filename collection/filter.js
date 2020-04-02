eb4b0f355cddcac6d4e39132c1220d19056b9603
dzendev <dzendev@gmail.com>
2020-04-02 12:33:48 +0400

    change collection/filter.js

--- collection/filter.js
+++ collection/filter.js
@@ -10,6 +10,12 @@ var users = [
   { 'user': 'fred',   'age': 40, 'active': false }
 ];
 
+// underscore
+console.log("und.filter(users, function(o) {return !o.active})", und.filter(users, function(o) {return !o.active}));
+console.log("und.filter(users, { 'age': 36, 'active': true })", und.filter(users, { 'age': 36, 'active': true }));
+console.log("und.filter(users, ['active', false])", und.filter(users, ['active', false])); // []
+console.log("und.filter(users, 'active')", und.filter(users, 'active'));
+
 // lodash
 console.log("lod.filter(users, function(o) {return !o.active})", lod.filter(users, function(o) {return !o.active}));
 console.log("lod.filter(users, { 'age': 36, 'active': true })", lod.filter(users, { 'age': 36, 'active': true }));
