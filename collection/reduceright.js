ff1fafc3d2ad4e2e80113834f78cc667e1c27c1a
dzendev <dzendev@gmail.com>
2020-04-02 12:27:17 +0400

    change collection/reduceright.js

--- collection/reduceright.js
+++ collection/reduceright.js
@@ -5,13 +5,19 @@ const lod = require('lodash');
  * reduce - возвращает результирующее значение, перебор происходит справа
  */
 
-
-// lodash
 var array = [[0, 1], [2, 3], [4, 5]];
-let arr1 = lod.reduceRight(array, function(flattened, other) {
+
+// underscore
+let arr1 = und.reduceRight(array, function(flattened, other) {
   return flattened.concat(other);
 }, []);
 console.log("arr1", arr1);
+
+// lodash
+let arr2 = lod.reduceRight(array, function(flattened, other) {
+  return flattened.concat(other);
+}, []);
+console.log("arr2", arr2);
 
 // es6
 console.log("array.reduceRight((flattened, other) => flattened.concat(other), [])", array.reduceRight((flattened, other) => flattened.concat(other), []));
