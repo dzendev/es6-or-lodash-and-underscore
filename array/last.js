ce1975f1398116d7485e0c4d95f24bb2bde6e27a
dzendev <dzendev@gmail.com>
2020-12-05 17:30:05 +0400

    change last.js

--- array/last.js
+++ array/last.js
@@ -17,4 +17,5 @@ console.log("lod.last(arr)", lod.last(arr));
 
 // es6
 console.log("arr[arr.length - 1]", arr[arr.length - 1]);
+console.log("arr.slice(-1)[0]", arr.slice(-1)[0]);
 console.log("arr.slice(-2)", arr.slice(-2)); // und.last(arr, 2))
